e75d635fb75e8b0bd4504d8827a457b85169dc8a
Olawale Abosede <abosedekikelomo408@gmail.com>
2026-05-05 00:05:27 -0400

    stagging test

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ app.MapGet("/", () => Results.Content("""
     <title>My Dockerized .NET App</title>
 </head>
 <body>
-    <h1>Hello from my .NET Docker App!</h1>
+    <h1>I am enjoying myself really well i love technical stuff</h1>
+    <h1>Hello from my .NET Docker Apphhhhhhh!</h1>
     <p>This app is running inside a container.</p>
 </body>
 </html>
